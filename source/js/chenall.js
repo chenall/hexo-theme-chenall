2b5ac329c16b68bf56773925e65b6fdb847d00e5
chenall <chenall.cn@gmail.com>
2014-03-31 14:08:02 +0800

    使用jquery的on事件来显示tooltip.

--- source/js/chenall.js
+++ source/js/chenall.js
@@ -1,19 +1,5 @@
 function loadjs(c,d){var a=document.createElement("script");a.async=!0;a.type="text/javascript";a.src=c;a.charset=d||"gbk";(document.getElementsByTagName("HEAD")[0]||document.getElementsByTagName("BODY")[0]).appendChild(a)};
 var _protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-function SetToolTip(obj){
-  obj = $(obj || 'body').find('a');
-  obj.mouseover(function(e){
-    this.myTitle = this.title || this.innerText;
-    this.myHref = decodeURI(this.href);
-//    this.myHref = (this.myHref.length > 30 ? this.myHref.toString().substring(0,35)+"..." : this.myHref);
-    this.title = "";
-    var tooltip = "<div id='tooltip'><p>"+this.myTitle+"<em>"+this.myHref+"</em>"+"</p></div>";
-    $('body').append(tooltip);
-    $('#tooltip').css({"opacity":"0.8","top":(e.pageY+20)+"px","left":(e.pageX-10)+"px"}).show('fast');
-  }).mouseout(function(){this.title = this.myTitle;$('#tooltip').remove();
-  }).mousemove(function(e){$('#tooltip').css({"top":(e.pageY+20)+"px","left":(e.pageX-10)+"px"});
-  });
-}
 
 function load_random_posts(obj){
   if (!obj) return;
@@ -24,13 +10,22 @@ function load_random_posts(obj){
   var html = '<ul class="list-group">';
   $.each(arr,function(index,item){html += '<li class="list-group-item"><a href="'+site.BASE_URI+ item.uri + '">' + (item.title || item.uri) + '</a></li>';});
   $(obj).html(html + '</ul>');
-  SetToolTip(obj);
 }
 
 jQuery(document).ready(function($){
-  SetToolTip();
+  $('body').on(
+    {mouseover:function(e){
+      this.myTitle = this.title || this.innerText;
+      this.myHref = decodeURI(this.href);
+      this.title = "";
+      var tooltip = "<div id='tooltip'><p>"+this.myTitle+"<em>"+this.myHref+"</em>"+"</p></div>";
+      $('body').append(tooltip);
+      $('#tooltip').css({"opacity":"0.8","top":(e.pageY+20)+"px","left":(e.pageX-10)+"px"}).show('fast');},
+    mouseout:function(){this.title = this.myTitle;$('#tooltip').remove();},
+    mousemove:function(e){$('#tooltip').css({"top":(e.pageY+20)+"px","left":(e.pageX-10)+"px"})}
+    },'a[href][href!="#"]');
   $('.content p:first-child').addClass("fp");
-  $('.ajax_widgets').each(function(){var src=$(this).attr('data-src');if(src)$(this).load(site.BASE_URI + 'widgets/'+src+'.html',function(){SetToolTip(this)});});
+  $('.ajax_widgets').each(function(){var src=$(this).attr('data-src');if(src)$(this).load(site.BASE_URI + 'widgets/'+src+'.html');});
   $.each(_js2load,function(index,obj){loadjs(obj.src,obj.charset)});
   $('.hexo_random_posts').each(function(){
     var c=this;
